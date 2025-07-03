1ccdaa37c8cc218db11dd1d062c4da50c36b6352
justajoo0374 <justajoo0374@gmail.com>
2025-07-03 21:47:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "google"  {
     project = "white-resolver-460502-v9"
 }
-resource "google_storage_bucket" "my-bucketss" {
-  name          = "bkt-demo-000478899"
+resource "google_storage_bucket" "my-bucketjuns" {
+  name          = "bkt-demo-june30"
   location      = "us-central1"
   project = "white-resolver-460502-v9"
   force_destroy = true
   public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "my" {
-  name          = "bkt-demo-000"
+resource "google_storage_bucket" "my-oook" {
+  name          = "bkt-demo-jun-897776"
   location      = "us-central1"
   project = "white-resolver-460502-v9"
   force_destroy = true
